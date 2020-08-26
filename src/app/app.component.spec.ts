8f6b887bdd75c94cc8d83694de84cfcdc8d70e25
Steve Zelek <szelek@chathamfinancial.com>
2020-08-26 14:33:15 -0400

    repro

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,28 +4,15 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [],
+    });
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'test-tests'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-tests');
-  });
-
-  it('should render title', () => {
+  fit(`should have as title 'test-tests'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-tests app is running!');
+
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('test-tests');
   });
 });
